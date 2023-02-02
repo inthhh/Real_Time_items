9fcf17ff2223e60ba846d8d26bba24f463cea435
inthhh <glass005533@gmail.com>
2023-02-02 15:36:07 +0900

    시계 구현

--- wAPI.js
+++ wAPI.js
@@ -40,4 +40,28 @@ fetch(apiUrl)
       iconClass = "04d";
     }
     weatherIconEl.innerHTML = `<img src='http://openweathermap.org/img/wn/${iconClass}.png'>`;
+    // 아이콘 변경 예정
   });
+
+var Target = document.getElementById("clock");
+var Target_apm = document.getElementById("apm");
+
+function clock() {
+  var time = new Date();
+  var hours = time.getHours();
+  var minutes = time.getMinutes();
+  var seconds = time.getSeconds();
+  var AmPm = "AM";
+  if (hours > 12) {
+    var AmPm = "PM";
+    hours %= 12;
+  }
+
+  Target.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${
+    seconds < 10 ? `0${seconds}` : seconds
+  }`;
+
+  Target_apm.innerText = `${AmPm}`;
+}
+clock();
+setInterval(clock, 1000); // 1초마다 실행
